dcd570a9b7da64274e6336db79abb8e1db00e5b4
Дмитрий Lert < Lert>
2017-04-06 19:02:58 +0400

    12

---
+++ СarriageСharacteristic.cs
@@ -0,0 +1,14 @@
+﻿namespace PTS
+{
+    public struct СarriageСharacteristic
+    {
+        public string type;                                //Тип вагона
+        double weight;                              //Масса
+        double baseLength;                          //Длина базы
+        double heightFromFloor;                     //Высота пола от Угр
+        double centerOfGravity;                     //Высота Цт от Угр
+        double length;                              //Длина 
+        double width;                               //Ширина
+        double windwardSurfaceArea;                 //Площадь наветренной поверхности
+    }
+}
---
+++ Сharacteristics.cs
@@ -0,0 +1,25 @@
+﻿namespace PTS
+{
+    struct Сharacteristics
+    {
+        string name;                                //Наименование
+        double weight;                              //Масса 
+        double length;                              //Длина 
+        double width;                               //Ширина
+        double height;                              //Высота
+        double centerOfGravity;                     //Высота ЦТ
+        double Lpr;                                 //Lпр
+        double Bp;                                  //Bп
+        double L_CT;                                //Расстояние от поперечного борта вагона до Цт
+        double B_CT;                                //Расстояние от продольного борта вагона до Цт
+        double coefficientOfFriction;               //коэф трения
+        double heightOfLongitudinal;                //Высота продольного  упора от основания груза
+        double heightOfTransverse;                  //Высота поперчного упора от основания груза
+        double windwardSurfaceArea;                 //Площадь наветренной поверхности
+        double heightAboveFloor;                    //Высота груза над полом вагона
+        double HeightOfProtruding;                  //Высота выступающей части от бортов полувагона
+        double coefficientOfFrictionTransverse;     //поперечный коэф трения
+        double additionalLongitudinalLoad;          //Дополнительная продольная нагрузка от грузов №
+        double additionalLateralLoad;               //Дополнительная поперечная нагрузка от грузов №
+    }
+}
